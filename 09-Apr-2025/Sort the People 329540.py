8663a6b82e6d7df599d370dedcae2cd38136bd39
Richard Elinam Nutsugah <61556574+eli-bigman@users.noreply.github.com>
2025-04-09 11:53:31 +0000

    Created 'Sort the People' by A2SV Hub

---
+++ 09-Apr-2025/Sort the People 329540.py
@@ -0,0 +1,14 @@
+# Problem: Sort the People - https://leetcode.com/problems/sort-the-people/
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(names)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if heights[i] < heights[j]:
+                    heights[i], heights[j] = heights[j], heights[i]
+                    names[i], names[j] = names[j], names[i]
+        return names
+
+
+
