1cd811bbaed95d125f4196fda74160b0720f578b
Richard Elinam Nutsugah <61556574+eli-bigman@users.noreply.github.com>
2025-04-14 10:21:07 +0000

    Created 'A - Destroyer' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #7 14-Apr-2025/A - Destroyer 333231.py
@@ -0,0 +1,30 @@
+# Problem: A - Destroyer - https://codeforces.com/gym/602997/problem/A
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = sorted(list(map(int, input().split())))
+
+    def destroyer(arr,n):
+        counter = dict(sorted(Counter(arr).items()))
+        keys = counter.keys()
+        max_n = arr[-1]
+        values = list(counter.values())
+
+        if len(set(counter.keys())) != (max_n + 1):
+            print("NO")
+            return
+        if not all(init >= prev for init,prev in zip(values, values[1:])):
+            print("NO")
+            return
+        print("YES")
+        
+    destroyer(arr,n)
+
+
+    
+    
+
+
+
