50c6546ed98b4ec39efd51362fdb7dbcc45a9fd6
Richard Elinam Nutsugah <61556574+eli-bigman@users.noreply.github.com>
2025-04-14 10:21:24 +0000

    Created 'B - Average Numbers' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #7 14-Apr-2025/B - Average Numbers 333232.py
@@ -0,0 +1,27 @@
+# Problem: B - Average Numbers - https://codeforces.com/gym/602997/problem/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+def find_average(arr,n):
+    average = sum(arr)/n
+    if int(average) < average:
+        print(0)
+        return 
+    else:
+        average = int(average)
+
+    count_avg = 0
+    idx_avg = []
+
+    for i in range(n):
+        if arr[i] == average:
+            count_avg += 1
+            idx_avg.append(i+1)
+
+    print(count_avg)
+    print(*sorted(idx_avg))
+    # return
+
+find_average(arr,n)
+
