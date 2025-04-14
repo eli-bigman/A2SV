646b6baf09cd562d41ec0c1e6e046568556104f3
Richard Elinam Nutsugah <61556574+eli-bigman@users.noreply.github.com>
2025-04-14 10:20:50 +0000

    Created 'E - Coins' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #7 14-Apr-2025/E - Coins 333230.py
@@ -0,0 +1,9 @@
+# Problem: E - Coins - https://codeforces.com/gym/602997/problem/E
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    
+    if n % 2 == 0 or k % 2 != 0:  
+        print("YES")
+    else:
+        print("NO")
